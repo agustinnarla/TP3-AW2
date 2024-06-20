a6310425a6066624a8780326cc44192d5e8417c8
agustin <agustin@arla.com.ar>
2024-06-20 11:45:50 -0300

    Nuevo

--- back/configuracion.js
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'
-export const {
-    PORT = 3000,
-    PGPORT,
-    PGDATABASE,
-    PGHOST,
-    PGPASSWORD,
-    PGUSER
-} = process.env
--- back/ruta/ruta.mjs
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { traerProductos,agregarProducto } from "../metodo/funciones.mjs";
-export const ruta = new Router()
-
-ruta.get("/productos",traerProductos)
-ruta.post("/productos",agregarProducto)
-
