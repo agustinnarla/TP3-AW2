30d26d799bc4bfbb8700974fedb4e61d2e03b69c
agustin <agustin@arla.com.ar>
2024-06-27 09:15:03 -0300

    Cambios en el front - terminado

--- front/src/index.js
+++ front/src/index.js
@@ -12,8 +12,8 @@ async function traerProductos() {
                 HTML += `
                     <article class="border-b border-gray-200 py-4">
                         <h3 class="text-lg font-semibold">${producto.nombre}</h3>
-                        <p class="text-gray-600">Marca: ${producto.marca}</p>
                         <p class="text-gray-600">Categoría: ${producto.categoria}</p>
+                        <p class="text-gray-600">Marca: ${producto.marca}</p>
                         <p class="text-gray-600">Stock: ${producto.stock}</p>
                         <a href="modificar.html?id=${producto.id}" class="text-blue-500 hover:text-blue-700">Modificar</a>
                         <a href="eliminar.html?id=${producto.id}" class="text-blue-500 hover:text-blue-700">Eliminar</a>
