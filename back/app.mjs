0e5c882e4beb498bc337bb92f8f2da5ba19002c6
agustin <agustin@arla.com.ar>
2024-06-28 10:33:17 -0300

    mensaje error

--- back/app.mjs
+++ back/app.mjs
@@ -9,9 +9,13 @@ const app = express()
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
-app.use('/',ruta)
 app.use(express.static('../front'))
+app.use('/',ruta)
 
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Ruta no encontrada' });
+    next()
+});
 
 const PORT = process.env.PORT ?? 3000
 
