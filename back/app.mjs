0c8dd5eb8d427f2be347c0415650fb45a8cb499f
agustin <agustin@arla.com.ar>
2024-06-28 10:43:23 -0300

    Error del helmet

--- back/app.mjs
+++ back/app.mjs
@@ -6,11 +6,14 @@ dotenv.config()
 
 const app = express()
 
+
+
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
 app.use(express.static('../front'))
 app.use('/',ruta)
+
 
 app.use((req, res, next) => {
     res.status(404).json({ error: 'Ruta no encontrada' });
